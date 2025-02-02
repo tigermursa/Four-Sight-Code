5456402fcf66a4e0dff9491a2d691cc375d4644b
Mursalin <mursalin2311@gmail.com>
2025-02-02 23:02:11 +0600

    first commit

---
+++ C Programming/Problem-001/hello_world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main( ){
+
+char name [20];
+
+printf("What should I call you?:");
+
+fgets(name,sizeof(name),stdin);
+printf("Hello  %s , How you doing today? \n",name);
+
+return 0;
+}
